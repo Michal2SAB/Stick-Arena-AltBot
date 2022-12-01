f0de43f181bab215638ed8420343324927f4611f
Michał <38110165+Michal2SAB@users.noreply.github.com>
2022-12-01 11:14:39 +0100

    Update AltBot60.py

--- 60-Kills-TOR/AltBot60.py
+++ 60-Kills-TOR/AltBot60.py
@@ -1,1 +1,1 @@
-// This worked with the labpass server as the kills limit was set to 60. Using TOR proxies we could bypass the IP limitations for one server and load 22 bots.
+## This worked with the labpass server as the kills limit was set to 60. Using TOR proxies we could bypass the IP limitations for one server and load 22 bots.
