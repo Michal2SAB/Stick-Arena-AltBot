7023a9b834e4e37be2f68ea07e49749b8d18f833
Michał <38110165+Michal2SAB@users.noreply.github.com>
2022-12-01 11:14:25 +0100

    Update AltBot40.py

--- 40-Kills/AltBot40.py
+++ 40-Kills/AltBot40.py
@@ -1,1 +1,1 @@
-// Since each server except labpass one had the 40 kill limit, there was no point dealing with bad TOR connections anymore. Instead we could spread the alting games across multiple servers, 4 in each.
+# Since each server except labpass one had the 40 kill limit, there was no point dealing with bad TOR connections anymore. Instead we could spread the alting games across multiple servers, 4 in each.
